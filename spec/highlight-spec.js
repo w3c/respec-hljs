3efb4ebc6adbc91c8dbf76da8be6f3e78d28c61b
Marcos Cáceres <mcaceres@mozilla.com>
2019-03-18 15:27:33 +1100

    BREAKING CHANGE: just export as hljs

--- spec/highlight-spec.js
+++ spec/highlight-spec.js
@@ -1,35 +1,32 @@
-const { respecHljs } = require("../dist/respec-highlight");
+const { hljs } = require("../dist/respec-highlight");
 
 describe("respec-highlight bundle", () => {
   it("highlights css", () => {
-    const { value, language } = respecHljs.highlightAuto(
-      `@import url(some.css);`,
-      ["css"]
-    );
+    const { value, language } = hljs.highlightAuto(`@import url(some.css);`, [
+      "css"
+    ]);
     expect(language).toBe("css");
     expect(value).toContain('<span class="hljs-keyword">import</span>');
   });
   it("highlights http", () => {
-    const { value, language } = respecHljs.highlightAuto(`Referer: me`, [
-      "http"
-    ]);
+    const { value, language } = hljs.highlightAuto(`Referer: me`, ["http"]);
     expect(language).toBe("http");
     expect(value).toContain('<span class="hljs-attribute">Referer</span>');
   });
   it("highlights javascript", () => {
-    const { value, language } = respecHljs.highlightAuto(`function test(){}`, [
+    const { value, language } = hljs.highlightAuto(`function test(){}`, [
       "javascript"
     ]);
     expect(language).toBe("javascript");
     expect(value).toContain('<span class="hljs-keyword">function</span>');
   });
   it("highlights json", () => {
-    const { value, language } = respecHljs.highlightAuto(`{"prop": "value"}`);
+    const { value, language } = hljs.highlightAuto(`{"prop": "value"}`);
     expect(language).toBe("json");
     expect(value).toContain('<span class="hljs-attr">"prop"</span>:');
   });
   it("highlights xml/html", () => {
-    const { value, language } = respecHljs.highlightAuto(
+    const { value, language } = hljs.highlightAuto(
       `<script type="application/javascript">`,
       ["html"]
     );
--- src/respec-highlight.js
+++ src/respec-highlight.js
@@ -1,22 +1,22 @@
+import * as highlight from "highlight.js/lib/highlight";
 import css from "highlight.js/lib/languages/css";
-import hljs from "highlight.js/lib/highlight";
 import http from "highlight.js/lib/languages/http";
 import javascript from "highlight.js/lib/languages/javascript";
 import json from "highlight.js/lib/languages/json";
 import xml from "highlight.js/lib/languages/xml";
 
-hljs.configure({
+highlight.configure({
   tabReplace: "  ", // 2 spaces
   languages: ["abnf", "css", "http", "javascript", "json", "xml"]
 });
 
-hljs.registerLanguage("css", css);
-hljs.registerLanguage("http", http);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("json", json);
-hljs.registerLanguage("xml", xml);
+highlight.registerLanguage("css", css);
+highlight.registerLanguage("http", http);
+highlight.registerLanguage("javascript", javascript);
+highlight.registerLanguage("json", json);
+highlight.registerLanguage("xml", xml);
 
-export const respecHljs = hljs;
+export const hljs = highlight;
 
 // For worker, as we can't ES6 import yet
-(module || self).respecHljs = hljs;
+(module || self).hljs = highlight;
